90184c7016ec8d878bd5f8e8356c64ddddaa9a18
Hyeonmin Seo <hmseo@me.com>
2022-02-06 22:20:47 +0900

    Initial Commit

---
+++ COVIDNumbers/COVIDNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  COVIDNumbersApp.swift
+//  COVIDNumbers
+//
+//  Created by Hyeonmin Seo on 2/6/22.
+//
+
+import SwiftUI
+
+@main
+struct COVIDNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
