e94b1af737b15b058e95fd051a7c1f8d51019bdd
Hyeonmin Seo <hmseo@me.com>
2022-02-09 20:20:13 +0900

    Part 1

---
+++ COVIDNumbers/Models/Worldwide.swift
@@ -0,0 +1,21 @@
+//
+//  Worldwide.swift
+//  COVIDNumbers
+//
+//  Created by Hyeonmin Seo on 2/8/22.
+//
+
+import Foundation
+
+struct Worldwide {
+    let updated: Int
+    let cases: Int
+    let todayCases: Int
+    let deaths: Int
+    let todayDeaths: Int
+    let recovered: Int
+    let todayRecovered: Int
+    let active: Int
+    let population: Int
+    let affectedCountries: Int
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# COVIDNumbers
+
+This project is related to the tutorial series of SwiftUI. You can see the tutorial on my blog written in Korean, The Hobbyist Programmer. Check the link below. This project uses **[NovelCOVID API](https://github.com/disease-sh/API)**.
+
+[The Hobbyist Programmer](https://hmseo.github.io)
